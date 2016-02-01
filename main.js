991fb9474a8e8b54cec18256730244c742439f7d
sbaldwin970 <sdbj93@gmail.com>
2016-02-01 10:17:05 -0700

    second commit

--- main.js
+++ main.js
@@ -5,55 +5,56 @@ var mainController = function($scope) {
 	var computerChoice = playArray[Math.floor(Math.random() * playArray.length)];
 	var result = "";
 
-	$scope.rock = function() {
+	var choseRock = function() {
 		if(computerChoice === playArray[1]) {
-			result = 'You lose';
+			result = 'You lose!';
 		}
 		else if(computerChoice === playArray[2]) {
-			result = 'You win';
+			result = 'you win!';
 		}
 		else {
-			result = 'Draw';
+			result = 'Draw!';
 		}
-		console.log(result);
-	}
-	
-	$scope.paper = function() {
+		return result;
+	};
+	var chosePaper = function() {
 		if(computerChoice === playArray[0]) {
-			result = 'You win';
+			result = 'You win!';
 		}
 		else if(computerChoice === playArray[2]) {
-			result = 'You Lose';
+			result = 'you lose!';
 		}
 		else {
-			result = 'Draw';
+			result = 'Draw!';
 		}
-		console.log(result);
+		return result;
 	}
-	
-	$scope.scissors = function() {
+	var choseScissors = function() {
 		if(computerChoice === playArray[0]) {
-			result = 'You lose';
+			result = 'You lose!';
 		}
 		else if(computerChoice === playArray[1]) {
-			result = 'You win';
+			result = 'you win!';
 		}
 		else {
-			result = 'Draw';
+			result = 'Draw!';
 		}
-		console.log(result);
+		return result;
 	}
 
+
+
+
+	$scope.showrock = function() {
+		$scope.rockResult = choseRock($scope.rock);
+	}
+	$scope.showpaper = function() {
+		$scope.paperResult = chosePaper($scope.paper);
+	}
+	$scope.showscissors = function() {
+		$scope.scissorsResult = choseScissors($scope.scissors);
+	}
 }
-
-
-
-
-
-	
-
-
-
 
 angular.module('rps')
 .controller('mainController', ['$scope', mainController]);
